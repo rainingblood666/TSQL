608d1f641dd6390043fdff7a6187a750e91a754d
rainingblood666 <stan.mischuk@gmail.com>
2020-03-22 15:12:49 +0200

    Update guery_mssqllog.sql

--- guery_mssqllog.sql
+++ guery_mssqllog.sql
@@ -1,4 +1,4 @@
-/* This is example how to query datetime of data changes, if audit routine wasn't set*/
+/* This is example how to query datetime of data changes, using sql server transaction log, if audit routine wasn't set*/
 
 USE [AdventureWorksDW2017]
 GO
