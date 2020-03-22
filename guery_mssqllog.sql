ae6b4c1f7c4544653c15fdfbe9e7f881106ef8e6
rainingblood666 <stan.mischuk@gmail.com>
2020-03-22 15:11:34 +0200

    Add files via upload

---
+++ guery_mssqllog.sql
@@ -0,0 +1,29 @@
+/* This is example how to query datetime of data changes, if audit routine wasn't set*/
+
+USE [AdventureWorksDW2017]
+GO
+
+UPDATE dbo.FactFinance
+SET Date=getdate() , Amount=Amount+1
+WHERE FinanceKey=1
+GO
+
+WITH main as(   
+    SELECT 
+    replace(replace(sys.fn_PhysLocFormatter (%%physloc%%),')',''),'(','') page,
+    FinanceKey,  Date, Amount
+    FROM dbo.FactFinance (NOLOCK)   
+    WHERE FinanceKey=1   )  ,
+     tlog as ( 
+    SELECT page, l2.[Begin Time],L2.[End Time],
+    l1.AllocUnitName l1_uname, l2.[Transaction Name] l2_tname,
+    l1.[Transaction ID]
+    FROM main
+join
+sys.fn_dblog(NULL,NULL) l1 on PATINDEX('%'+main.page+'%',l1.[Lock Information] )>0
+left join sys.fn_dblog(NULL,NULL) l2 on l2.[Transaction ID]=l1.[Transaction ID]  
+  )
+SELECT PAGE,[Transaction ID], min([Begin Time]),min([End Time]),min(l1_uname),min(l2_tname)
+FROM tlog
+GROUP BY [Transaction ID],page
+
